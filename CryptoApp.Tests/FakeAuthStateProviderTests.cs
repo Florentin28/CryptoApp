7aa4f82b95cac06627d16db5a7b8623d5d430409
Florentin Muraille <muraillef@gmail.com>
2026-03-13 22:58:39 +0100

    correction bug persistance

--- CryptoApp.Tests/FakeAuthStateProviderTests.cs
+++ CryptoApp.Tests/FakeAuthStateProviderTests.cs
@@ -1,21 +1,28 @@
 using Bunit;
 using Xunit;
 using CryptoApp.Services;
+using Microsoft.JSInterop;
 
 namespace CryptoApp.Tests;
 
-// On utilise BunitContext pour simuler facilement le navigateur
 public class FakeAuthStateProviderTests : BunitContext
 {
     [Fact]
     public async Task Login_DoitConnecterLUtilisateur()
     {
         // 1. Arrange
-        this.JSInterop.Mode = JSRuntimeMode.Loose; // Autorise l'usage du LocalStorage simulé
+        this.JSInterop.Mode = JSRuntimeMode.Loose; 
+
+        // ON UTILISE LA MÊME CLÉ QUE DANS LE SERVICE : "currentUser"
+        this.JSInterop.Setup<string>("localStorage.getItem", "currentUser")
+                      .SetResult("Jean Michel");
+
         var authProvider = new FakeAuthStateProvider(this.JSInterop.JSRuntime);
 
         // 2. Act
         await authProvider.LoginAsync("Jean Michel");
+        
+        // On récupère l'état (qui va aller lire "currentUser" dans le faux LocalStorage)
         var state = await authProvider.GetAuthenticationStateAsync();
 
         // 3. Assert
